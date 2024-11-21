cff760f171d785c311928158b690085042a055e2
Jinwoo Park <jpark286@myseneca.ca>
2024-11-21 02:05:13 -0500

    Update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // server.js
 
 /*********************************************************************************
-WEB322 – Assignment 03
+WEB322 – Assignment 04
 I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
 No part of this assignment has been copied manually or electronically from any other 
 source (including 3rd party web sites) or distributed to other students.
